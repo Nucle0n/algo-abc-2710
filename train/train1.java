ac0aa29234d910904745795f75a85e3718814f2e
Nucleon <thibaud.guinchard@gmail.com>
2025-12-03 12:47:57 +0100

    formatage du code

--- train/train1.java
+++ train/train1.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 
 public class train1
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         float a , b , c ;
         Scanner sc = new Scanner (System.in) ;
+
         System.out.print("Entrez a : ") ;
         a = sc.nextFloat() ; 
         System.out.print("Entrez b : ") ;
         b = sc.nextFloat() ;
         System.out.print("Entrez c : ") ;
         c = sc.nextFloat() ;
+
         float result = (a * b) / c ;
+
         System.out.println("Le résultat est : " + result);
+        
         sc.close();
                 
     }
--- train/train2.java
+++ train/train2.java
@@ -3,10 +3,12 @@ package train;
 import java.util.Scanner;  
 
 public class train2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         boolean a, b, c;
         boolean result;
+
         System.out.print("Entrez la valeur de a (true/false) : ");    
         a = sc.nextBoolean();
         System.out.print("Entrez la valeur de b (true/false) : ");
@@ -14,7 +16,9 @@ public class train2 {
         System.out.print("Entrez la valeur de c (true/false) : ");
         c = sc.nextBoolean();
         result = a || b && !c;
+
         System.out.println("Etat en sortie = " + result);
+        
         sc.close();
     }
     }
